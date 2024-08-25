f4175db766c7a2e99fdb274f50b7f12c496186a7
Anne Cahalan <northofnormal@gmail.com>
2024-08-25 15:42:26 -0400

    Initial Commit

---
+++ SwitfLearnings/SwitfLearningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfLearningsApp.swift
+//  SwitfLearnings
+//
+//  Created by Anne Cahalan on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwitfLearningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
